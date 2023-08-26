ac6bc1408318ce9b69f0eb78a015812b9f9043e4
Nirupama Jha <ptjmniru@gmail.com>
2023-08-26 16:17:31 +0530

    Added inputs and buttons

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
 from dash import Dash, dcc, html
 # import dash_core_components as dcc
 # import dash_html_components as html
-from datetime import datetime as dt
+from datetime import date
 
 app=Dash(__name__)
 
 server=app.server
 app.layout = html.Div([
-    html.H1('Hello Dash'),
     html.Div([
-        html.P('Welcome to the stock dash app! ', className="start"),
+        html.H1('Welcome to the stock dash app! ', className="start"),
         html.Div([
-            # data range picker input
+            html.P('Input Stock Code'),
+            dcc.Input(id="stockCode", type="text", placeholder="stockCode", style={'marginRight':'10px'}),
+        ]),
+        html.Div([
+            dcc.DatePickerSingle(
+                month_format='MMM Do, YY',
+                placeholder='start->',
+                date=date(2017, 6, 21),
+                className="datePicker",
+            )
         ]),
         html.Div([
             # Stock price button
+            html.Button('Stock Price', id='btn-Stock-Price'),
             # Indicators button
+            html.Button('Indicators', id='btn-Indicator'),
             # Number of days of forecast input
+            dcc.Input(id="numOfDays", type="text", placeholder="Number of Days ", style={'marginRight':'10px'}),
             # Forecast button
+            html.Button('Forecast', id='btn-Forecast'),
         ])
-    ], className="nav"),
+    ], className="inputs"),
     html.Div([
         html.Div([
             #LOGO
@@ -40,7 +52,7 @@ app.layout = html.Div([
         ], id="forecast-content")
     ],className="Content"),
     
-])
+],className="container")
 
 
 if __name__ == '__main__':
